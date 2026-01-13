ca2affccb19f88de19e339ef41032619307ac5ab
jawad <jawadfoot.2019@gmail.com>
2026-01-13 15:39:26 +0100

    Add items to carts

--- src/main/java/com/jawad/store/Controllers/CartController.java
+++ src/main/java/com/jawad/store/Controllers/CartController.java
@@ -1,15 +1,20 @@
 package com.jawad.store.Controllers;
 
+import com.jawad.store.dtos.AddItemToCartRequest;
 import com.jawad.store.dtos.CartDto;
+import com.jawad.store.dtos.CartItemDto;
 import com.jawad.store.entities.Cart;
+import com.jawad.store.entities.CartItem;
 import com.jawad.store.mappers.CartMapper;
 import com.jawad.store.repositories.CartRepository;
+import com.jawad.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
@@ -18,6 +23,7 @@ public class CartController {
 
     private final CartRepository cartRepository;
     private final CartMapper cartMapper;
+    private final ProductRepository productRepository;
 
     @PostMapping
     public ResponseEntity<CartDto> createCart(UriComponentsBuilder uriBuilder) {
@@ -28,6 +34,49 @@ public class CartController {
         //return new ResponseEntity<>(cartDto, HttpStatus.CREATED);
         var uri=uriBuilder.path("/carts/{id}").buildAndExpand(cartDto.getId());
         return ResponseEntity.created(uri.toUri()).body(cartDto);
+    }
+
+    @PostMapping("/{cartId}/items")
+    public ResponseEntity<CartItemDto> addToCart(@PathVariable UUID cartId,
+                                                 @RequestBody AddItemToCartRequest request)
+    {
+        var cart=cartRepository.findById(cartId).orElse(null);
+        if(cart==null){
+            return ResponseEntity.notFound().build();
+        }
+        var product=productRepository.findById(request.getProductId()).orElse(null);
+
+        if(product==null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        //find product if already in cart ,if yes only add quantity ,if not add it to cart
+
+        var cartItem=cart.getCartItems()
+                .stream()
+                .filter(i->i.getProduct().getId().equals(product.getId()))
+                .findFirst()
+                .orElse(null);
+        if(cartItem!=null){
+            cartItem.setQuantity(cartItem.getQuantity()+1);
+        }else{
+            //cartItem can be null, not cartitem type
+            cartItem=new CartItem();
+            cartItem.setQuantity(1);
+            cartItem.setProduct(product);
+            cartItem.setCart(cart);
+            cart.getCartItems().add(cartItem);
+        }
+
+        cartRepository.save(cart);
+
+        var cartItemDto=cartMapper.toDto(cartItem);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
+
+
+
+
     }
 
 }
--- src/main/java/com/jawad/store/mappers/CartMapper.java
+++ src/main/java/com/jawad/store/mappers/CartMapper.java
@@ -1,10 +1,17 @@
 package com.jawad.store.mappers;
 
 import com.jawad.store.dtos.CartDto;
+import com.jawad.store.dtos.CartItemDto;
 import com.jawad.store.entities.Cart;
+import com.jawad.store.entities.CartItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
     CartDto maptoDto(Cart cart);
+
+    //mapp totalPrice field to a method (or another field if not same name)
+    @Mapping(target = "totalPrice",expression = "java(cartItem.getTotalPrice())")
+    CartItemDto toDto(CartItem cartItem);
 }
