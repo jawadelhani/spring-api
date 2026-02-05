381e740304f0fcb4df5d19c74a1eefe1586e98d6
Jawad <jawadfoot.2019@gmail.com>
2026-02-05 23:17:11 +0100

    working with AuthenticationManager

--- src/main/java/com/jawad/store/repositories/UserRepository.java
+++ src/main/java/com/jawad/store/repositories/UserRepository.java
@@ -4,6 +4,11 @@ import com.jawad.store.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsUserByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
