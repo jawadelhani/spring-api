25e05d9a48a874d46827cf33425c98746b2dd11f
Jawad <jawadfoot.2019@gmail.com>
2026-02-05 17:44:45 +0100

    Hash password

--- src/main/java/com/jawad/store/config/SecurityConfig.java
+++ src/main/java/com/jawad/store/config/SecurityConfig.java
@@ -14,6 +14,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
